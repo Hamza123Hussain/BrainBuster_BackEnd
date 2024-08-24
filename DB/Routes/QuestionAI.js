6fe4a3ef082e0c094cfaa88ba1e05099f14fb202
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 17:26:28 +0500

    getting all of the quizez ever made

--- DB/Routes/QuestionAI.js
+++ DB/Routes/QuestionAI.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { AiQuestionController } from '../Controllers/QuestionsAsk.js'
+
+import { getAllAiQuestions } from '../Controllers/Fetch/GettingAllDocs.js'
+import { AiQuestionController } from '../Controllers/Create/QuestionsAsk.js'
 
 const QuestionAiRouter = express.Router()
 
 QuestionAiRouter.post('/', AiQuestionController)
-
+QuestionAiRouter.get('/GetAll', getAllAiQuestions)
+/**UserID as qury in the get */
 export default QuestionAiRouter
