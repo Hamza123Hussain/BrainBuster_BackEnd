02f08872caef38727469f565a6891580c790c742
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 17:25:04 +0500

    unused code has been removed

--- DB/Routes/SummaryRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { SummaryController } from '../Controllers/SummaryAi.js'
-
-const SummaryRouter = express.Router()
-
-SummaryRouter.post('/', SummaryController)
-
-export default SummaryRouter
