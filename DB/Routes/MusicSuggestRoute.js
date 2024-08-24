dc1d142f119a65fe0512bd78cd896d478a5df8e5
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-24 06:20:53 +0500

    removed all unuused files

--- DB/Routes/MusicSuggestRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { MusicController } from '../Controllers/MusicSugget.js'
-
-const MusicRouter = express.Router()
-
-MusicRouter.post('/', MusicController)
-
-export default MusicRouter
