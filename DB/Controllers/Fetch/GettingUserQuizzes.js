9cf742c3b68dff4e0f86a226e27fddedc09e9354
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 22:04:30 +0500

    this gets all the quizzes from the firebase

--- DB/Controllers/Fetch/GettingUserQuizzes.js
+++ DB/Controllers/Fetch/GettingUserQuizzes.js
@@ -1,39 +1,26 @@
-import { collection, getDocs, query } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import { DB } from '../../../Firebase.js'
-
-const sanitizeEmail = (email) => {
-  return email.replace(/[^a-zA-Z0-9_]/g, '_') // Replace non-alphanumeric characters with underscores
-}
 
 export const GetAiQuestionsController = async (req, res) => {
   const { UserEmail } = req.query
-
   try {
     if (!UserEmail) {
       return res.status(400).json({ error: 'UserEmail is required' })
     }
-
-    const sanitizedUserEmail = sanitizeEmail(UserEmail)
-
-    // Reference to the user's AiQuestion collection
-    const aiQuestionsCollectionRef = collection(
-      DB,
-      'AiQuestionSuggest',
-      sanitizedUserEmail,
-      'AiQuestion'
+    // Reference to the AIQUIZ collection
+    const aiQuestionsCollectionRef = collection(DB, 'AIQUIZ')
+    // Create a query to filter documents by UserEmail
+    const aiQuestionsQuery = query(
+      aiQuestionsCollectionRef,
+      where('UserEmail', '==', UserEmail)
     )
-
-    // Query to get all documents in the AiQuestion collection
-    // const aiQuestionsQuery = query(aiQuestionsCollectionRef)
-
-    const querySnapshot = await getDocs(aiQuestionsCollectionRef)
-
+    // Get documents matching the query
+    const querySnapshot = await getDocs(aiQuestionsQuery)
     // Extract data from query results
     const aiQuestions = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }))
-
     if (aiQuestions.length > 0) {
       res.status(200).json(aiQuestions)
     } else {
