0229c3cdcce436fddd28fe056efc2f83a9ac6c6d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 16:46:49 +0500

    added the image middleware in the update router

--- DB/Routes/UserRouter.js
+++ DB/Routes/UserRouter.js
@@ -2,12 +2,15 @@ import express from 'express'
 import { UpdateUserController } from '../Controllers/Update/UpdateUser.js'
 import { deleteAiQuestion } from '../Controllers/Delete/DeleteQuiz.js'
 import { GetAiQuestionsController } from '../Controllers/Fetch/GettingUserQuizzes.js'
+import multer from 'multer'
+import { GetUserController } from '../Controllers/Fetch/GettingUserDetails.js'
 
 const UserRouter = express.Router()
-
-UserRouter.put('/UpdateDoc', UpdateUserController)
+const upload = multer({ storage: multer.memoryStorage() })
+UserRouter.put('/UpdateUser', upload.single('image'), UpdateUserController)
 UserRouter.delete('/DeleteQuiz', deleteAiQuestion)
 UserRouter.get('/GetQuiz', GetAiQuestionsController)
+UserRouter.get('/GetUser', GetUserController)
 //  UserEmail as query in the get
 /**UserID, RandomID as query in the delete */
 
