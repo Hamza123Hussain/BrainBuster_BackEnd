3ccece9f425a5319e9298dc842a1c9c652246615
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-25 16:10:05 +0500

    sending the image as the middleware to the register router

--- DB/Routes/AuthRouter.js
+++ DB/Routes/AuthRouter.js
@@ -2,10 +2,11 @@ import express from 'express'
 import { SignUpController } from '../Controllers/Auth/SignUP.js'
 import { LoginController } from '../Controllers/Auth/Login.js'
 import { SignOutController } from '../Controllers/Auth/SignOut.js'
+import multer from 'multer'
 
 const AuthRouter = express.Router()
-
-AuthRouter.post('/', SignUpController)
+const upload = multer({ storage: multer.memoryStorage() })
+AuthRouter.post('/', upload.single('image'), SignUpController)
 AuthRouter.post('/Login', LoginController)
 AuthRouter.get('/', SignOutController)
 
