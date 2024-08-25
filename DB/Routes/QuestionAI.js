b6052a2bd4b9cbb3d2967e69132b04b28bb2fd89
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-26 03:27:45 +0500

    giving the router an api path for a single quiz

--- DB/Routes/QuestionAI.js
+++ DB/Routes/QuestionAI.js
@@ -2,10 +2,13 @@ import express from 'express'
 
 import { getAllAiQuestions } from '../Controllers/Fetch/GettingAllDocs.js'
 import { AiQuestionController } from '../Controllers/Create/QuestionsAsk.js'
+import { GetDoc } from '../Controllers/Fetch/GettingASingleQuiz.js'
 
 const QuestionAiRouter = express.Router()
 
 QuestionAiRouter.post('/', AiQuestionController)
 QuestionAiRouter.get('/GetAll', getAllAiQuestions)
+QuestionAiRouter.get('/GetDoc', GetDoc)
+//ID as query in getdoc
 /**UserID as qury in the get */
 export default QuestionAiRouter
