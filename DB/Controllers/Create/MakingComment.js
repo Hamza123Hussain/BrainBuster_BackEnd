94c505e5ecf6046903e5cb033ceecbb4870e98ee
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 15:14:02 +0500

    sending complete comment data to the frontend

--- DB/Controllers/Create/MakingComment.js
+++ DB/Controllers/Create/MakingComment.js
@@ -4,25 +4,29 @@ import { DB } from '../../../Firebase.js'
 export const create_commment = async (req, res) => {
   const randomId = uuidv4()
   try {
-    const { message, UserEmail, QUIZID } = req.body
+    const { message, UserEmail, QUIZID, UserImage, UserName } = req.body
     // Fetch the user document from Firestore
     const userDoc = await getDoc(doc(DB, 'USERS', UserEmail))
     if (userDoc.exists()) {
-      const User = userDoc.data()
       // Update the post document with the new comment
       await updateDoc(doc(DB, 'AIQUIZ', QUIZID), {
         comments: arrayUnion({
           CommentID: randomId,
           Text: message,
-          UserName: User.Name,
+          UserName,
           UserEmail,
           CreatedAt: Date.now(),
-          UserImage: User.imageUrl,
+          UserImage,
         }),
       })
-      res
-        .status(200)
-        .json({ success: true, message: 'Comment added successfully' })
+      res.status(200).json({
+        CommentID: randomId,
+        Text: message,
+        UserName,
+        UserEmail,
+        CreatedAt: Date.now(),
+        UserImage,
+      })
     } else {
       res.status(404).json({ success: false, message: 'User not found' })
     }
