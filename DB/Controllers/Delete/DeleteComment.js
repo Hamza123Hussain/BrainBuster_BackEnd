f0c0537ca8e87545ed476098370be7ee598c22e6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 15:42:57 +0500

    sending the response as true only

--- DB/Controllers/Delete/DeleteComment.js
+++ DB/Controllers/Delete/DeleteComment.js
@@ -4,31 +4,29 @@ export const deletecomment = async (req, res) => {
   try {
     const { QUIZID, CommentID } = req.query
 
-    console.log('Received QUIZID:', QUIZID)
-    console.log('Received CommentID:', CommentID)
+    // console.log('Received QUIZID:', QUIZID)
+    // console.log('Received CommentID:', CommentID)
 
     // Fetch the post document from Firestore
     const postDoc = await getDoc(doc(DB, 'AIQUIZ', QUIZID))
 
     if (postDoc.exists()) {
       const postData = postDoc.data()
-      console.log('Current Post Data:', postData)
+      // console.log('Current Post Data:', postData)
 
       // Ensure comments is an array and filter out the comment to be deleted
       if (Array.isArray(postData.comments)) {
         const updatedComments = postData.comments.filter(
           (comment) => comment.CommentID !== CommentID
         )
-        console.log('Updated Comments:', updatedComments)
+        // console.log('Updated Comments:', updatedComments)
 
         // Update the post document with the new comments array
         await updateDoc(doc(DB, 'AIQUIZ', QUIZID), {
           comments: updatedComments,
         })
 
-        res
-          .status(200)
-          .json({ success: true, message: 'Comment deleted successfully' })
+        res.status(200).json(true)
       } else {
         res
           .status(400)
