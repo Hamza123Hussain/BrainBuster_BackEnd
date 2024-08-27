0fd1f0cd926eb96f6b999e965aa0266d3236fd3a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-27 16:11:20 +0500

    ADDING NEW PATH TO THE ROUTER

--- DB/Routes/UserRouter.js
+++ DB/Routes/UserRouter.js
@@ -4,13 +4,14 @@ import { deleteAiQuestion } from '../Controllers/Delete/DeleteQuiz.js'
 import { GetAiQuestionsController } from '../Controllers/Fetch/GettingUserQuizzes.js'
 import multer from 'multer'
 import { GetUserController } from '../Controllers/Fetch/GettingUserDetails.js'
-
+import { UserAttempts } from '../Controllers/Create/UserAttemptedQuiz.js'
 const UserRouter = express.Router()
 const upload = multer({ storage: multer.memoryStorage() })
 UserRouter.put('/UpdateUser', upload.single('image'), UpdateUserController)
 UserRouter.delete('/DeleteQuiz', deleteAiQuestion)
 UserRouter.get('/GetQuiz', GetAiQuestionsController)
 UserRouter.get('/GetUser', GetUserController)
+UserRouter.post('/UserAttempts', UserAttempts)
 //  UserEmail as query in the get
 /**UserID, RandomID as query in the delete */
 
